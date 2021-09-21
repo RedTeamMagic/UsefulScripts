808e4776f6ef3917e23f72838a9fa0314494ed6a
Red Team Magic <48022272+RedTeamMagic@users.noreply.github.com>
2021-09-21 16:22:16 -0400

    Create grouper.sh

---
+++ grouper.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#firefox & 
+
+input=$1
+lines=$(wc -l <$input)
+counter=0
+var=10
+zero=0
+
+echo Total lines: $lines
+
+while IFS= read -r line
+do
+        let "counter++"
+        #host $line
+        echo Opening: $line
+        current=$(expr $lines - $counter)
+        echo $current
+        firefox --new-tab $line
+
+        breakcheck=$(expr $counter % $var)
+        echo $breakcheck
+        echo $zero
+        if [ $breakcheck -eq $zero ]
+        then
+                read -p "press any key to continue..." < /dev/tty
+        fi
+
+done < "$input"
