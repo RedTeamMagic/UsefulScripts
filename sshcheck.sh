35b14746e630aa7fa6efdd779b58928cdf65accd
Red Team Magic <48022272+RedTeamMagic@users.noreply.github.com>
2023-04-22 19:35:36 -0400

    Create sshcheck.sh

---
+++ sshcheck.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# define the IP address or hostname to check
+HOST="127.0.0.1"
+
+# loop through every port number from 1 to 65535
+for PORT in $(seq 1 65535); do
+  # use ssh to connect to the host on the current port
+ ssh -4 -p "$PORT" "$HOST"
+  # check the exit status of the ssh command
+  if [ $? -eq 0 ]; then
+    # if the ssh connection was successful, print the port number
+    echo "SSH connection successful on port $PORT"
+  fi
+done
