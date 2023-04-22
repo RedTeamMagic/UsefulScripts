96ad2e6ab9c1101284c9ecbff45f860da27a728e
Red Team Magic <48022272+RedTeamMagic@users.noreply.github.com>
2023-04-22 19:54:09 -0400

    Update sshcheck.sh

--- sshcheck.sh
+++ sshcheck.sh
@@ -4,7 +4,7 @@
 HOST="127.0.0.1"
 
 # loop through every port number from 1 to 65535
-for PORT in $(seq 1 65535); do
+for PORT in $(seq 34460 65535); do
   # use ssh to connect to the host on the current port
  ssh -4 -p "$PORT" "$HOST"
   # check the exit status of the ssh command
