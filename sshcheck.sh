d785ee689d87cdb7b332d1a72520d0b2dd2b43bc
Red Team Magic <48022272+RedTeamMagic@users.noreply.github.com>
2023-04-23 00:10:05 -0400

    Update sshcheck.sh

--- sshcheck.sh
+++ sshcheck.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 for port in {1..65535}; do
-  ssh -q -o BatchMode=yes -o ConnectTimeout=1 localhost -p $port "echo 2>/dev/null" >/dev/null 2>&1
-  if [ $? -eq 0 ]; then
-    echo "SSH is running on port $port"
-  fi
+  status=$(ssh -q -o BatchMode=yes -o ConnectTimeout=1 localhost -p $port "echo 2>/dev/null" >/dev/null 2>&1 && echo "open" || echo "closed")
+  echo "Port $port is $status"
 done
