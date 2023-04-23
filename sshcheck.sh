652261fc1643d9214c1467ea1b7ffad35d4555d1
Red Team Magic <48022272+RedTeamMagic@users.noreply.github.com>
2023-04-23 00:03:35 -0400

    Update sshcheck.sh

--- sshcheck.sh
+++ sshcheck.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-# define the IP address or hostname to check
-HOST="127.0.0.1"
-
-# loop through every port number from 1 to 65535
-for PORT in $(seq 34460 65535); do
-  # use ssh to connect to the host on the current port
- ssh -4 -p "$PORT" "$HOST"
-  # check the exit status of the ssh command
+for port in {1..65535}; do
+  ssh -q -o BatchMode=yes -o ConnectTimeout=1 localhost -p $port "echo 2>/dev/null" >/dev/null 2>&1
   if [ $? -eq 0 ]; then
-    # if the ssh connection was successful, print the port number
-    echo "SSH connection successful on port $PORT"
+    echo "SSH is running on port $port"
   fi
 done
